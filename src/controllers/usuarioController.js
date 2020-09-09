1eacacf10e06cc23822f6c8ea2e8dc71e5cef0f3
FelipeLeonardo98 <felipeferrazleonardo98@gmail.com>
2020-09-09 20:58:23 -0300

    Backend com SelectAll e SelectDetail

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,6 +1,7 @@
 // Setting the use of Model Usuario and dependency http=status
 const Usuario = require('../models/usuario');
 const status = require('http-status');
+const { NOT_FOUND } = require('http-status');
 
 // Create method Insert, getting datas received of Request
 exports.Insert = (req, res, next) => {
@@ -28,3 +29,32 @@ exports.Insert = (req, res, next) => {
         //catch = register that we want happend when Promise is failed
         .catch(error => next("An error has ocurred: " + error));
 };
+
+// Create method SelectAll (Select * from ...)
+exports.SelectAll = (req, res, next) => {
+    Usuario.findAll()
+        .then(usuario => {
+            if (usuario) {
+                res.status(status.OK).send(usuario);
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next("Sorry, an error ocurred with SelectAll: " + error));
+
+};
+
+exports.SelectDetail = (req, res, next) => {
+    const id = req.params.id;
+
+    Usuario.findByPk(id)
+        .then(usuario => {
+            if (usuario) {
+                res.status(status.OK).send(usuario);
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next("Sorry, an error ocurred when findById was requisited: " + error));
+};
+
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 // route '/usuarios' getting the method 'Insert' from UsuarioController
 router.post('/usuarios', UsuarioController.Insert);
-
+router.get('/usuarios', UsuarioController.SelectAll);
+router.get('/usuarios/:id', UsuarioController.SelectDetail);
 
 module.exports = router;
